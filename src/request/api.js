19b57cdafb7314693fc63c0ccaae23dccc4b451f
soumah yaya <soumah_yaya@hotmail.com>
2022-12-20 11:18:07 +0800

    data analysis report complete

--- src/request/api.js
+++ src/request/api.js
@@ -1,9 +1,15 @@
 import axios from 'axios'
 import {baseURL} from '../util/constant'
+import NProgress from 'nprogress'
 
 axios.defaults.baseURL = baseURL;
 axios.interceptors.request.use((config)=>{
+    NProgress.start()
     config.headers.Authorization = window.localStorage.getItem('token')
+    return config
+})
+axios.interceptors.response.use((config)=>{
+    NProgress.done()
     return config
 })
 export default axios
